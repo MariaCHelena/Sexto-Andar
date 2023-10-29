225fb78a5ae7d0b5773f2f38df2acbf0966ae44a
Maria <marianenacarvalho@gmail.com>
2023-10-29 00:40:35 -0300

    Criando pastas do projeto

---
+++ sextoAndar/src/main/feedImoveis.java
@@ -0,0 +1,17 @@
+package main;
+
+public class feedImoveis {
+	private int quantidadeDeImoveis;
+	
+	public void exibirImoveis() {
+		return;
+	}
+	
+	public void filtrarFeedDeImoveis(String tipoDeImovel, double preco) {
+		return;
+	}
+	
+	public void ordenarFeed() {
+		return;
+	}
+}
