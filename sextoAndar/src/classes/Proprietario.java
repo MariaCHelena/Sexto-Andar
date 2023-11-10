0353de0ec5dbe63cee030d75401553ec7407c424
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2023-11-10 15:12:34 -0300

    Criando arraylist na classe de Proprietário

--- sextoAndar/src/classes/Proprietario.java
+++ sextoAndar/src/classes/Proprietario.java
@@ -1,26 +1,32 @@
 package classes;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Proprietario extends Conta{
-	private int qtdImoveis;
-	private Imovel[] imoveisCadastrados = new Imovel[3];
+	private List<Imovel> imoveisCadastrados = new ArrayList<>();
 
 	public Proprietario(String nomeUsuario, String numeroCelular, String email, String senha) {
 		super(nomeUsuario, numeroCelular, email, senha);
-		this.qtdImoveis = 0;
-	}
-	
-	public int getQtdImovies() {
-		return this.qtdImoveis;
 	}
 	
 	public boolean cadastrarImovel(Imovel i) {
-		if(qtdImoveis < 3) {
-			imoveisCadastrados[qtdImoveis] = i;
-			qtdImoveis++;
-			return true;
-		} else {
-			return false;
+		imoveisCadastrados.add(i);
+		return true;
+	}
+	
+	public String exibirImoveisCadastrados() {
+		String mensagem = "";
+		for (Imovel i : imoveisCadastrados) {
+			mensagem += i;
 		}
+		return mensagem;
+	}
+	
+	public String toString() {
+		return "Conta de Proprietário" + "------------------------\n" + "Nome do usuário cadastrado: " + this.nomeUsuario + 
+				"\nNúmero do celular cadastrado: " + this.numeroCelular +
+				"\nEmail da conta: " + this.email + 
+				"\nQuantidade de imóveis cadastrados na conta: " + imoveisCadastrados.size();
 	}
 	
 }
