021e5e648fd820e521bcfe4b8fcf5caa9a5bc1e1
m4rllon <marllonpc33@gmail.com>
2023-11-19 09:49:16 -0300

    gets&setsPropostaMarllon

--- sextoAndar/src/negocio/Proposta.java
+++ sextoAndar/src/negocio/Proposta.java
@@ -12,6 +12,30 @@ public class Proposta {
 		this.autor = autor;
 		this.imovel = imovel;
 	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public double getValorProposta() {
+		return valorProposta;
+	}
+	public void setValorProposta(double valorProposta) {
+		this.valorProposta = valorProposta;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public Imovel getImovel() {
+		return imovel;
+	}
+	public void setImovel(Imovel imovel) {
+		this.imovel = imovel;
+	}
 	
 	
 }
